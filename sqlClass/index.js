9a55856bcc26c797e6f1c9731a0106143b6b172e
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-13 17:23:48 +0530

    code modified

--- sqlClass/index.js
+++ sqlClass/index.js
@@ -8,25 +8,29 @@ const connection = mysql.createConnection({
   password: "abhi7631@@",
 });
 
-let q = "INSERT INTO user (id,username,email,password) VALUES (?,?,?,?)";
+let getRandomUser = () => {
+  return [
+    faker.string.uuid(),
+    faker.internet.userName(),
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+};
+
+let q = "INSERT INTO user (id,username,email,password) VALUES ?";
+
+let data = [];
+for (let i = 1; i <= 100; i++) {
+  data.push(getRandomUser());
+}
 
 try {
-  connection.query(q, (err, result) => {
+  connection.query(q, [data], (err, result) => {
     if (err) throw err;
     console.log(result);
-    console.log(result.length);
   });
 } catch (err) {
   console.log(err);
 }
 
 connection.end();
-
-let getRandomUser = () => {
-  return {
-    id: faker.string.uuid(),
-    username: faker.internet.userName(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-  };
-};
