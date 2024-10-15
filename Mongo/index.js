86bccca481ebd5f9a50c512888c034fac66b069f
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-15 10:42:57 +0530

    code modified

--- Mongo/index.js
+++ Mongo/index.js
@@ -18,17 +18,29 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
-User.findByIdAndUpdate(
-  "670de0d124597e9b7c9dc8fd",
-  { name: "chandan" },
-  { new: true }
-)
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.error("Error:", err);
-  });
+User.findByIdAndDelete("670df905944f00863328c804").then((res) => {
+  console.log(res);
+});
+
+// User.deleteMany({ age: { $lt: 48 } }, { new: true }).then((res) => {
+//   console.log(res);
+// });
+
+// User.deleteOne({ name: "chandan" }).then((res) => {
+//   console.log(res);
+// });
+
+// User.findByIdAndUpdate(
+//   "670de0d124597e9b7c9dc8fd",
+//   { name: "chandan" },
+//   { new: true }
+// )
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.error("Error:", err);
+//   });
 
 // User.findOne({ age: { $gt: 47 } })
 //   .then((res) => {
