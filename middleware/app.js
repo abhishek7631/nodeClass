9726e29accdba080c4f878cf3638b56848768635
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-19 09:00:02 +0530

    code modified

--- middleware/app.js
+++ middleware/app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res) => {
+  console.log("Hi, i am a middleware");
+  res.send("middleware finished");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hi, i am root");
+});
+
+app.get("/random", (req, res) => {
+  res.send("this is a random page");
+});
+
 app.listen(8080, () => {
   console.log("server is listing");
 });
