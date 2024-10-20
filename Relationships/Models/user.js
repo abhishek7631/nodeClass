2f4899ee312747e3a0df97b97c82cea1fa29f93a
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-20 20:06:07 +0530

    one to few implementation

--- Relationships/Models/user.js
+++ Relationships/Models/user.js
@@ -1,1 +1,41 @@
 const mongoose = require("mongoose");
+
+main()
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/relationDemo");
+}
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  address: [
+    {
+      _id: false,
+      location: String,
+      city: String,
+    },
+  ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+const addUsers = async () => {
+  let user1 = new User({
+    username: "abhishek",
+    address: [
+      {
+        location: "221B Baker street ",
+        city: "London",
+      },
+    ],
+  });
+  user1.address.push({ location: "P32 DownTown", city: "London" });
+  let result = await user1.save();
+  console.log(result);
+};
+
+addUsers();
